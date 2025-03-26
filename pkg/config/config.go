2ad211e83ccfdab55aac1a931168a083aa65239d
MTUCIBOY <miha150rus@yandex.ru>
2025-03-26 22:18:55 +0300

    Write begining config pkg

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,15 +1,62 @@
+// Пакет config нужен для работы с конфигурацией облачного сервера.
+// Пакет использует конфиги в формате YAML.
+// Зависит от стороннего пакета cleanenv для более удобной работы с тегами структуры и парсинга конфиг-файла.
 package config
 
-import "time"
+import (
+	"log"
+	"os"
+	"time"
 
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// Config Структура исходных настроек облачного сервера.
 type Config struct {
-	Env        string `env-default:"local" json:"env"`
-	StorageDns string `env-required:"true" json:"storage_dns"`
-	HTTPServer `json:"http_server"`
+	// Переменная окружения.
+	// Обычно ставится как local, но в перспективе может быть как debug и prod.
+	// Нужна для разного уровня логирования.
+	Env string `env-default:"local" yaml:"env"`
+
+	// Переменная для подключения к базе данных.
+	// Используется в GORM PostgresSQL.
+	// Если строка пустая, но приложение не запустится.
+	StorageDNS string `env-required:"true" yaml:"storage_dns"`
+
+	// Настройки HTTP-сервера.
+	HTTPServer `yaml:"http_server"`
 }
 
+// HTTPServer Структура настроек сервера.
 type HTTPServer struct {
-	Address     string        `env-default:"localhost:8888" json:"address"`
-	Timeout     time.Duration `env-default:"4s"             json:"timeout"`
-	IdleTimeout time.Duration `env-default:"60s"            json:"idle_timeout"`
+	// Адресс, на котором будет запускаться приложение.
+	// Обычно - localhost:8888.
+	Address string `env-default:"localhost:8888" yaml:"address"`
+
+	// Время на чтение запроса и отправку ответа.
+	Timeout time.Duration `env-default:"4s" yaml:"timeout"`
+
+	// Время жизни соединения с клиентом.
+	IdleTimeout time.Duration `env-default:"60s" yaml:"idle_timeout"`
+}
+
+// MustLoad Функция парсинга конфигурационного файла.
+// Путь до файла читается из переменной окружения CONFIG_PATH.
+// В случае любой ошибки, приложение падает.
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("failed parse config file: %v", err)
+	}
+
+	return &cfg
 }
